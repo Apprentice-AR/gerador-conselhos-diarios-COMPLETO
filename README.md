af1778779497fb12493990ff2f3df163e51a7e34
André Ramos Paes Prieto <arpp9@outlook.com>
2025-10-07 04:16:21 -0300

    Criação e upload do README.md e dos demais arquivos

---
+++ README.md
@@ -0,0 +1,2 @@
+"# Gerador de Conselhos Di rios COMPLETO" 
+"# Programa gerador de conselhos di rios" 
---
+++ scriptATV2.js
@@ -0,0 +1,7 @@
+const conselhos = [
+    "Amigo(a), relaxa, TEMOS FEIJOADA!",
+    "Todo dia é um novo dia para um novo você.",
+    "E TAMBÉM TEMOS PÃO DE QUEIJO!!",
+    "Ainda dá tempo, se esforce sempre!"
+];
+console.log("Arquivo de script carregado. Array de conselhos pronto.");
